413f922e86e682f28bfa9590ef89335f1a816fe0
Leonardo._ <148483527+hamsterbr@users.noreply.github.com>
2025-09-29 02:59:13 +0000

    tudo feito

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function openPopup(id){document.getElementById(id).classList.remove('hidden');}
 function closePopup(id){document.getElementById(id).classList.add('hidden');}
 function switchPopup(current,next){closePopup(current);openPopup(next);}
+function cadastrarAluno() {
+  // Aqui você pode adicionar validações antes se quiser
+  closePopup('aluno-cadastro'); // fecha o popup
+  // redireciona para a página de montar currículo
+  window.location.href = "curriculo.html";
+}
+function cadastrarEmpresa() {
+  // aqui você pode adicionar validações se necessário
+  closePopup('empresa-cadastro'); // fecha o popup
+  // redireciona para a página de cadastro de empresa
+  window.location.href = "empresa-cadastro.html";
+}
